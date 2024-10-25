2978251a61d98797b7c73b66989e9bfbe75e5ba0
Abiel Paré <104913520+AbielPare@users.noreply.github.com>
2024-10-25 10:35:34 -0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,18 @@
 <?php
 
+Use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return response() -> json([
         'message' => 'OK'
+    ]);
+});
+
+Route::get('/somar' , function (Request $request) {
+    $soma = array_sum($request->all());
+    return response() -> json([
+        'message' => 'Somado com sucesso' , //Opcional
+        'sum' => $soma,
     ]);
 });
